802700bff07ae7bdb074bb36d1f0937fee5aab85
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-04-06 23:33:10 +0430

    add

--- 1/src/App.js
+++ 1/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import AllItems from "./components/AllItems";
+import SelectedItems from "./components/SelectedItems";
+import ItemContextProvider from "./components/ItemContextProvider";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="container">
+    <header>
+      <ItemContextProvider>
+        <AllItems/>
+        <SelectedItems/>
+      </ItemContextProvider>
+    </header>
+  </div>
+
   );
 }
 
